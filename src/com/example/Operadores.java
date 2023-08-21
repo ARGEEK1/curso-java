ab28dae32c48ca351fcba68aeaea2c4cc2930bd0
ARGEEK1 <antonio198889@gmail.com>
2023-08-21 20:27:21 +0200

    operadores

---
+++ src/com/example/Operadores.java
@@ -0,0 +1,26 @@
+package com.example;
+
+public class Operadores {
+    public static void main(String[] args) {
+
+        //operadores aritmetricos
+        int num1 = 10;
+        int num2 = 20;
+
+        int result = num1 + num2;
+
+        System.out.println(result);
+
+        //comparacion : <, >, <=, >=, ==
+
+        boolean resultado1 = num1 > num2; //false
+        boolean resultado2 = num1 < num2; //true
+
+        System.out.println(resultado2);
+
+        //Logicos and && y or ||
+
+
+
+    }
+}
