0ba9e6a7a5f38ff1ae8671366b3945c8ee5dcf09
ARGEEK1 <antonio198889@gmail.com>
2023-08-21 18:09:20 +0200

    curso java

---
+++ src/com/example/HolaMundo.java
@@ -0,0 +1,22 @@
+package com.example;
+
+/**
+ * Clase de prueba para crear un programa basico en Java.
+ */
+public class HolaMundo {
+
+    /**
+     * Punto de entrada a la aplicacion Java
+     * permite ejecutar ala aplicacion
+     * @param args
+     */
+    public static void main(String[] args) {
+        //Esto es un comentario
+        System.out.println("Hello Word");
+        /*Esto es un comentario multilinea:
+        1 - crear funcion suma
+        2 - crear variables de tipo numerico
+        */
+
+    }
+}
---
+++ src/com/example/Tipos.java
@@ -0,0 +1,35 @@
+package com.example;
+
+public class Tipos {
+    public static void main(String[] args) {
+
+
+        // 1. numericos
+
+            // 1.1 enteros
+                byte variable1 = 20;
+                short variable2 = 10;
+                int variable3 = 30; //*
+                long variable4 = 100; //*
+                //numerios enteros, la diferencia es la capacidad,
+                // van de menos a mayor.
+
+            // 1.2 decimales
+                float variable5 = 5.5f;
+                double variable6 = 10.10d; //*
+                // numeros desimales, la diferencia es la capacidad,
+                // y se coloca la letra f o d como sufijo.
+
+        // 2. booleanos
+
+            boolean variable7 = false;
+            boolean variable8 = true;
+
+        // 3. texto
+
+            char variable9 = 'a';
+            String variable10 = "Hola Mundo"; //String es una clase, las anteriores son de tipo primitivo, por eso se escriben en minuscula.
+            // las variables de tipos primitivos no podran ser null.
+
+    }
+}
