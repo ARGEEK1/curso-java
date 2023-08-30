f7eaa262744918bd22d39ccb39c0d21961d32bbb
ARGEEK1 <antonio198889@gmail.com>
2023-08-30 20:35:43 +0200

    do while

---
+++ src/com/example/estructurascontrol/repeticion/DoWhile.java
@@ -0,0 +1,13 @@
+package com.example.estructurascontrol.repeticion;
+
+public class DoWhile {
+    public static void main(String[] args) {
+
+        int contador = 1;
+
+        do {
+            System.out.println("Número: " + contador);
+            contador++;
+        } while (contador <= 5);
+    }
+}
