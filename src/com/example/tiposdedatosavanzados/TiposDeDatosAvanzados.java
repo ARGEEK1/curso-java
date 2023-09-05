7056a9222202677786fb9e3db3fc6a68dcae2d2d
ARGEEK1 <antonio198889@gmail.com>
2023-09-06 01:19:16 +0200

    update

--- src/com/example/tiposdedatosavanzados/TiposDeDatosAvanzados.java
+++ src/com/example/tiposdedatosavanzados/TiposDeDatosAvanzados.java
@@ -15,11 +15,32 @@ public class TiposDeDatosAvanzados {
 
         int cadenaLen = cadena.length();
         String cadenaMinus = cadena.toLowerCase();
-        String cadenaMayu = cadena.toUpperCase(); 
+        String cadenaMayu = cadena.toUpperCase();
+        boolean resultado = cadena.startsWith("Men");//este metodo arroja true o false y es sensible a las mayusculas.
+        boolean result = cadena.endsWith("o");// evalua si termina en 'o' en este caso es verdadero.
+        char letra = cadena.charAt(0);// este metodo, le pasas un indice y te da como resultado, la letra que ocupa dicha posicion.
+        //Recorrer cadena
+        for (int i = 0; i < cadena.length(); i++) {
+            System.out.println("Caracter actual: " + cadena.charAt(i));
+        }
 
-
-
-
-        System.out.println(cadenaMayu);
+        System.out.println(cadenaInvertida2(cadena));
     }
+
+    static String cadenaInvertida(String cadena) {
+        String newCadena = "";
+        for (int i = cadena.length() - 1; i >= 0; i--) {
+            newCadena += cadena.charAt(i);
+        }
+        return newCadena;
+    }
+
+    static String cadenaInvertida2(String cadena) {
+        StringBuilder resultado = new StringBuilder();
+        for (int i = cadena.length() - 1; i >= 0; i--) {
+            resultado.append(cadena.charAt(i));
+        }
+        return resultado.toString();
+    }
+
 }
