737654c339fe0df1cc530c705187d1f26e6c79fe
ARGEEK1 <antonio198889@gmail.com>
2023-09-06 20:16:58 +0200

    update curso

--- src/com/example/tiposdedatosavanzados/TiposDeDatosAvanzados.java
+++ src/com/example/tiposdedatosavanzados/TiposDeDatosAvanzados.java
@@ -52,15 +52,40 @@ public class TiposDeDatosAvanzados {
         System.out.println(nombres.length);//A de los String .length no es un metodo, si no una
         // propieda, por eso se coloca sin el llamado().
         for (String i : nombres) {
-            System.out.println(i );
+            System.out.println(i);
         }
         // si ya tenemos un array declarado y de forma implicita o explicita
         // le decimos que vamos a tener 4 elementos por ejemplo, despues no puedes añadir
         // 5to elememto.
 
+        //Array bidimencionales
+        //1er elemmento el numero de filas, el 2do numero de columnas.
+        // los 2d se utilizan para asociaciones y los 3d para matrices matemáticas.
+        int arrayBidi[][] = new int[2][4];
 
+        arrayBidi[0][0] = 1;
+        arrayBidi[0][1] = 2;
+        arrayBidi[0][2] = 3;
+        arrayBidi[0][3] = 4;
 
+        arrayBidi[1][0] = 10;
+        arrayBidi[1][1] = 20;
+        arrayBidi[1][2] = 30;
+        arrayBidi[1][3] = 40;
 
+        for (int i = 0; i < arrayBidi.length; i++) {
+            System.out.println("estoy en la fila = " + i);
+            for (int j = 0; j < arrayBidi[i].length; j++) {
+                System.out.println(arrayBidi[i][j]);
+            }
+        }
+
+        // Cuando si lo sabemos sus valores
+        // 2 filas X 4 columnas
+        int arrayBidiDos[][] = {
+                {1, 3, 3, 4},
+                {10, 20, 30, 40}
+        };
     }
 
     static String cadenaInvertida(String cadena) {
