7a4b2e87732867bcbca45e1c380fa4eef6ff33f7
ARGEEK1 <antonio198889@gmail.com>
2023-10-16 15:53:20 +0200

    update

--- src/com/example/tiposdedatosavanzados/EntradaSalida.java
+++ src/com/example/tiposdedatosavanzados/EntradaSalida.java
@@ -10,9 +10,18 @@ public class EntradaSalida {
         try {
             InputStream fichero = new FileInputStream("/etc/passwd"); //Este metodo es para leer ficheros
 
-            byte[] datos;//el FileImputStream me devuele una secuencia de Bytes, por lo tanto, hay que guardarlo en una vadiable de tipo de datos acorde, y las secuencias son arrays
+            //byte[] datos;//el FileImputStream me devuele una secuencia de Bytes, por lo tanto, hay que guardarlo en una vadiable de tipo de datos acorde, y las secuencias son arrays
+            int dato;
             try {
-                datos = fichero.readAllBytes();
+                dato = fichero.read();
+                while (dato != -1) { // leer mientras.
+                    System.out.print((char) dato);
+                    dato = fichero.read();
+                }
+
+                //for (byte dato : datos) {
+                //System.out.print((char) dato);// se forza el tipo de datos, porque los bytes son numeros.
+                //}
             } catch (IOException e) {
                 System.out.println("No puedo leer el fichero: " + e.getMessage());
             }
