3b7ef89ebdb22100ae12accf4b618b67d1a0d652
ARGEEK1 <antonio198889@gmail.com>
2023-10-16 15:27:00 +0200

    add class Entrada y Salida

---
+++ src/com/example/tiposdedatosavanzados/EntradaSalida.java
@@ -0,0 +1,25 @@
+package com.example.tiposdedatosavanzados;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class EntradaSalida {
+    public static void main(String[] args) {
+        try {
+            InputStream fichero = new FileInputStream("/etc/passwd"); //Este metodo es para leer ficheros
+
+            byte[] datos;//el FileImputStream me devuele una secuencia de Bytes, por lo tanto, hay que guardarlo en una vadiable de tipo de datos acorde, y las secuencias son arrays
+            try {
+                datos = fichero.readAllBytes();
+            } catch (IOException e) {
+                System.out.println("No puedo leer el fichero: " + e.getMessage());
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("El programa da Error: " + e.getMessage());
+        }
+
+    }
+}
--- src/com/example/tiposdedatosavanzados/Mapas.java
+++ src/com/example/tiposdedatosavanzados/Mapas.java
@@ -1,7 +1,36 @@
 package com.example.tiposdedatosavanzados;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Mapas {
     public static void main(String[] args) {
+        //Un mapa recibe muchos nombre; los doa más comunes son
+        //mapas y array asociativo
+        //un mapa es una especie de diccionario, donte tenemis una clave
+        // y un valor. Ese valor podria ser otro map, bigdecimal, string, array, etc.
+        //La forma más primitiva del mapa es el HashMap.
+        HashMap<String, Integer> map = new HashMap<>();
+        //Este es un map donde las claves seran cadenas de texto y el valor numeros enteros.
+        //En los HashMap no se puede tener claves duplicadas. Porque el valor se sobreescribe.
+        map.put("clave1", 10);
+        map.put("clave2", 20);
+        map.put("clave3", 30);
+        map.put("clave4", 40);
+
+        System.out.println(map);
+        //sutituir valore:
+
+        map.put("clave1", 90);
+        map.replace("clave1", 100);// la clave tiene que excistir.
+        map.remove("clave1");
+
+        for (Map.Entry<String, Integer> elemento : map.entrySet()) {
+            System.out.println("Elemento clave es: " + elemento.getKey());
+            System.out.println("Elemento valor es: " + elemento.getValue());
+        }
+
+
 
     }
 }
